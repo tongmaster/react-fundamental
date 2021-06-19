f83464c06d2feb3b8779652486b41e1aa761c1b9
tongmaster <basfers@gmail.com>
2021-06-19 17:28:17 +0700

    darkMode actions.js Header.js Layout.js reducer.js for ui

--- ui/src/modules/ui/components/Header.js
+++ ui/src/modules/ui/components/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   AppBar,
   Toolbar,
   Link,
-  Switch,
   FormControlLabel,
-  IconButton,
+  Switch,
   Badge,
+  IconButton,
 } from "@material-ui/core";
-import logo from "assets/images/logo.png";
+import { Link as RouterLink, useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { ShoppingCart } from "@material-ui/icons";
-import { Link as RouterLink, useHistory } from "react-router-dom";
+
+import logo from "assets/images/logo.png";
+import * as actions from "../actions";
+
 const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer,
@@ -20,39 +24,57 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   logoImage: {
-    height: 30,
-    width: 30,
+    widht: "30px",
+    height: "30px",
   },
   spacer: {
     flexGrow: 1,
   },
 }));
+
 export default function Header() {
   const classes = useStyles();
   const history = useHistory();
+  const dispatch = useDispatch();
+  const darkMode = useSelector((state) => state.ui.darkMode);
+
   const navigateToCart = () => history.push("/cart");
+
+  const toggleDarkMode = () => dispatch(actions.toggleDarkMode());
+
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
         <Link
           component={RouterLink}
-          className={classes.logoLink}
           to="/"
           color="inherit"
           underline="none"
+          className={classes.logoLink}
         >
-          <img src={logo} alt="Shopping" className={classes.logoImage} />
+          <img src={logo} alt="Babel Shopping" className={classes.logoImage} />
         </Link>
-
-        <Link component={RouterLink} to="/products" color="inherit" underline="none">
+        <Link
+          component={RouterLink}
+          to="/products"
+          color="inherit"
+          underline="none"
+        >
           Products
         </Link>
         <div className={classes.spacer}></div>
         <FormControlLabel
-          control={<Switch color="secondary"></Switch>}
+          control={
+            <Switch
+              color="secondary"
+              checked={darkMode}
+              onChange={toggleDarkMode}
+            />
+          }
           label="Dark"
+          labelPlacement="end"
         ></FormControlLabel>
-        <IconButton color="inherit">
+        <IconButton color="inherit" onClick={navigateToCart}>
           <Badge badgeContent={5} color="secondary">
             <ShoppingCart></ShoppingCart>
           </Badge>
--- ui/src/modules/ui/reducer.js
+++ ui/src/modules/ui/reducer.js
@@ -1,7 +1,15 @@
-const initialState = {};
+import { TOGGLE_DARK_MODE, SET_DARK_MODE } from "./actions";
+
+const initialState = {
+  darkMode: false,
+};
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case TOGGLE_DARK_MODE:
+      return { ...state, darkMode: !state.darkMode };
+    case SET_DARK_MODE:
+      return { ...state, darkMode: action.payload.darkMode };
     default:
       return state;
   }
